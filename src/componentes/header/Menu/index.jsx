264acf2a3e71be8df954e9c078e37624da2e7ce3
Renato França <renatofranca2881@gmail.com>
2024-01-02 19:51:27 -0300

    comecando com header

---
+++ src/componentes/header/Menu/index.jsx
@@ -0,0 +1,9 @@
+const Menu = () =>{
+    return (
+        <>
+            <h1>Esse é o menu</h1>
+        </>
+    )
+}
+
+export default Menu;
---
+++ src/componentes/header/index.jsx
@@ -0,0 +1,11 @@
+import styles from './Header.module.css';
+
+const Header = () => {
+  return (
+    <header className={styles.conteiner}>
+      <h1>Olá mundo</h1>
+    </header>
+  );
+};
+
+export default Header;
